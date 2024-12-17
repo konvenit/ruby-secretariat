f900c54d0dff21e0bc902bda8f612e415e060efb
ahmedm10 <ahmed.mohammed.m10@gmail.com>
2024-12-17 19:21:23 +0200

    handle different categories for same tax percent

--- lib/secretariat/invoice.rb
+++ lib/secretariat/invoice.rb
@@ -67,6 +67,7 @@ module Secretariat
         taxes[line_item.tax_percent] = Tax.new(tax_percent: BigDecimal(line_item.tax_percent)) if taxes[line_item.tax_percent].nil?
         taxes[line_item.tax_percent].tax_amount += BigDecimal(line_item.tax_amount)
         taxes[line_item.tax_percent].base_amount += BigDecimal(line_item.net_amount) * line_item.quantity
+        taxes[line_item.tax_percent].category_code = line_item.tax_category
       end
       taxes.values
     end
@@ -223,6 +224,7 @@ module Secretariat
                 end
               end
               taxes.each do |tax|
+                self.tax_category = tax.category_code
                 xml['ram'].ApplicableTradeTax do
                   Helpers.currency_element(xml, 'ram', 'CalculatedAmount', tax.tax_amount, currency_code, add_currency: version == 1)
                   xml['ram'].TypeCode 'VAT'
